8f1c065d9383d31540dee5b8d0de9666a627de2f
julia-rom <jromanowski@sparkgeo.com>
2022-04-22 14:09:32 -0400

    hooks test in app js

--- mrtt-ui/src/App.test.tsx
+++ mrtt-ui/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders MRTT UI text', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  const mrttText = screen.getByText(/MRTT/i)
+  expect(mrttText).toBeInTheDocument()
 })
--- mrtt-ui/src/App.tsx
+++ mrtt-ui/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 function App(): JSX.Element {
+  const hookTest = ''
+  const hookTest2 = ''
   return <div className="App">MRTT UI</div>
 }
 
