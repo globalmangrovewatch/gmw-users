7f6fac3ee42e3675a6033fa550fd32b462e7c1c7
julia-rom <jromanowski@sparkgeo.com>
2022-04-26 14:26:36 -0400

    add select drop down

--- mrtt-ui/src/components/ProjectDetailsForm.js
+++ mrtt-ui/src/components/ProjectDetailsForm.js
@@ -3,15 +3,15 @@ import styles from './style.module.scss'
 import { useForm, Controller } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as Yup from 'yup'
-// import Box from '@mui/material/Box'
-import { TextField, Checkbox, FormLabel } from '@mui/material'
+import { TextField, Checkbox, FormLabel, InputLabel, Select, MenuItem } from '@mui/material'
 
 function ProjectDetailsForm() {
   // form validation rules
   const validationSchema = Yup.object().shape({
     projectTitle: Yup.string().required('Project title is required'),
     projectAims: Yup.array().of(Yup.string()).typeError('Select at least one item'),
-    projectDuration: Yup.number().required('Number is required').typeError('Must be a number')
+    projectDuration: Yup.number().required('Number is required').typeError('Must be a number'),
+    projectDurationUnit: Yup.string().required('Project period is required')
   })
   const formOptions = { resolver: yupResolver(validationSchema) }
 
@@ -27,8 +27,9 @@ function ProjectDetailsForm() {
     <div className="project-details-form">
       <h1>Project Details Form</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
+        {/* Project Title */}
         <div className={styles.formGroup}>
-          <label className={styles.label}>Project title</label>
+          <InputLabel sx={{ color: 'black' }}>Project title</InputLabel>
           <Controller
             name="projectTitle"
             control={control}
@@ -45,8 +46,11 @@ function ProjectDetailsForm() {
           />
           <div className={styles.invalid}>{errors.projectTitle?.message}</div>
         </div>
+        {/* Project Aims */}
         <div className={styles.formGroup}>
-          <label>What is the overall aim for the project area?</label>
+          <InputLabel sx={{ color: 'black' }}>
+            What is the overall aim for the project area?
+          </InputLabel>
           {options.map((value) => (
             <FormLabel key={value}>
               <Checkbox
@@ -59,21 +63,43 @@ function ProjectDetailsForm() {
             </FormLabel>
           ))}
           <div className={styles.invalid}>{errors.projectAims?.message}</div>
-          <div className={styles.formGroup}>
-            <label className={styles.label}>Project duration</label>
-            <input
-              defaultValue={0}
-              type="text"
-              {...register('projectDuration', { required: true })}
-            />
-            <div className={styles.invalid}>{errors.projectDuration?.message}</div>
-            <label className={styles.label}>Period</label>
-            <input
-              defaultValue="month(s)"
-              type="text"
-              {...register('projectDurationUnit', { required: true })}
-            />
-          </div>
+        </div>
+        {/* Project Duration */}
+        <div className={styles.formGroup}>
+          <InputLabel sx={{ color: 'black' }}>Project duration</InputLabel>
+          <Controller
+            name="projectDuration"
+            control={control}
+            defaultValue=""
+            render={({ field }) => (
+              <TextField
+                required
+                id="outlined-required"
+                label="Required"
+                variant="outlined"
+                {...field}
+              />
+            )}
+          />
+          <div className={styles.invalid}>{errors.projectDuration?.message}</div>
+          {/* Project Period */}
+          <InputLabel sx={{ color: 'black' }}>Period</InputLabel>
+          <Controller
+            name="projectDuration"
+            control={control}
+            render={({ field }) => (
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Period"
+                defaultValue="Months(s)"
+                {...field}>
+                <MenuItem value="months">Month(s)</MenuItem>
+                <MenuItem value="years">Year(s)</MenuItem>
+              </Select>
+            )}
+          />
+          <div className={styles.invalid}>{errors.projectDurationUnit?.message}</div>
         </div>
 
         <input type="submit" />
