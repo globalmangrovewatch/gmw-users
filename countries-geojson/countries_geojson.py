34366f8ae3ff63b078566f600cabc36b41b66c57
Chloe Papalazarou <cpapalazarou@sparkgeo.com>
2022-05-16 14:07:53 -0700

    added filtering of countries that contain mangroves =1

--- countries-geojson/countries_geojson.py
+++ countries-geojson/countries_geojson.py
@@ -1,4 +1,5 @@
 import csv, json
+from re import M
 from geojson import Feature, FeatureCollection, Point
 
 csvFilePath = "/Users/cpapalaz/Desktop/Mangrove_Countries.csv"
@@ -11,8 +12,10 @@ with open(csvFilePath, newline='', encoding='latin1') as csvfile:
     reader = csv.reader(csvfile, dialect=csv.excel, delimiter=',')
     next(reader, None)  # skip the headers
     for Country,Mangroves,Latitude,Longitude,MIN_POINT_X,MAX_POINT_X,MIN_POINT_Y,MAX_POINT_Y in reader:
-        Latitude, Longitude = map(float, (Latitude, Longitude))
-        features.append(
+        if Mangroves[0] == "1":
+            print(Mangroves[0])
+            Latitude, Longitude = map(float, (Latitude, Longitude))
+            features.append(
             Feature(
                 bbox= [MIN_POINT_X, MIN_POINT_Y, MAX_POINT_X, MAX_POINT_Y],
                 geometry = Point((Longitude, Latitude)),
@@ -22,6 +25,10 @@ with open(csvFilePath, newline='', encoding='latin1') as csvfile:
                 }
             )
         )
+        else:
+            pass
+
+
 
 collection = FeatureCollection(features)
 with open(jsonFilePath, "w") as f:
