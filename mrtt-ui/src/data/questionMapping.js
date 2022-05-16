062bbf47433119b0e2e3d259fd36a18616dbfb0c
julia-rom <jromanowski@sparkgeo.com>
2022-05-16 09:39:17 -0400

    add question mapping for section two

--- mrtt-ui/src/data/questionMapping.js
+++ mrtt-ui/src/data/questionMapping.js
@@ -5,5 +5,16 @@ export const questionMapping = {
     projectEndDate: '1.1c',
     countries: '1.2',
     siteArea: '1.3'
+  },
+  siteBackground: {
+    stakeholders: '2.1',
+    managementStatus: '2.2',
+    lawStatus: '2.3',
+    managementArea: '2.4',
+    protectionStatus: '2.5',
+    areStakeholdersInvolved: '2.6',
+    governmentArrangement: '2.7',
+    landTenture: '2.8',
+    customaryRights: '2.9'
   }
 }
