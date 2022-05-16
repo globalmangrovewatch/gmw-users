667aa29d929a0f3d6ba8a354dff925d02daa287f
julia-rom <jromanowski@sparkgeo.com>
2022-05-16 16:02:15 -0400

    fix path in projectDetails + update comments in utils function

--- mrtt-ui/src/library/mapDataForApi.js
+++ mrtt-ui/src/library/mapDataForApi.js
@@ -1,14 +1,14 @@
 import { questionMapping } from '../data/questionMapping'
 
-// set up data structure for api
+// set up data structure for api using question_id and answer_value object
 // ensure formTitle is the same is title in questionMapping
 export const mapDataForApi = (formTitle, data) => {
   const preppedData = []
+
   for (const [key, value] of Object.entries(data)) {
-    // map question ids with keys
     // longer version of Object.hasOwnProperty
-    // checks if key exists in question mapping section, then gets value of that key to use with question_id
     if (Object.prototype.hasOwnProperty.call(questionMapping[formTitle], key)) {
+      // map question ids with descriptive question keys
       preppedData.push({ question_id: questionMapping[formTitle][key], answer_value: value })
     }
   }
