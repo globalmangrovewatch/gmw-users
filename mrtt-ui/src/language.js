4e409fcf0bd9a941d8bae2e6094fd76846c02448
Mel Nunes <nunes.melissa.m@gmail.com>
2022-05-17 11:53:48 -0600

    Chore: ErrorText, SectionFormTitle, ButtonSubmit, Form reusable components. Language.js for reusable language (#61)

---
+++ mrtt-ui/src/language.js
@@ -0,0 +1,7 @@
+const error = { submit: 'Submit failed, please try again.' }
+const form = {
+  checkboxGroupOtherLabel: 'Other',
+  checkboxGroupOtherInputPlaceholder: 'If other, please state.'
+}
+
+export default { error, form }
