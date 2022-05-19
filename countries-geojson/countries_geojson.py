e8b9e362d028b3eae957195e6204c311285c3bb3
Chloe Papalazarou <cpapalazarou@sparkgeo.com>
2022-05-19 13:24:38 -0700

    formatting with black

--- countries-geojson/countries_geojson.py
+++ countries-geojson/countries_geojson.py
@@ -4,28 +4,34 @@ from geojson import Feature, FeatureCollection, Point
 csv_file_path = "Mangrove_Countries.csv"
 json_file_path = "mrtt-ui/src/data/mangrove_countries.json"
 
-#read csv file and add to data
+# read csv file and add to data
 features = []
-with open(csv_file_path, newline='', encoding='latin1') as csvfile:
-    reader = csv.reader(csvfile, dialect=csv.excel, delimiter=',')
+with open(csv_file_path, newline="", encoding="latin1") as csvfile:
+    reader = csv.reader(csvfile, dialect=csv.excel, delimiter=",")
     next(reader, None)  # skip the headers
     for row in reader:
-        Country, Mangroves, Latitude, Longitude, MIN_POINT_X, MAX_POINT_X, MIN_POINT_Y, MAX_POINT_Y = row
+        (
+            Country,
+            Mangroves,
+            Latitude,
+            Longitude,
+            MIN_POINT_X,
+            MAX_POINT_X,
+            MIN_POINT_Y,
+            MAX_POINT_Y,
+        ) = row
         if Mangroves[0] == "1":
             Latitude, Longitude = map(float, (Latitude, Longitude))
             features.append(
-            Feature(
-                bbox= [MIN_POINT_X, MIN_POINT_Y, MAX_POINT_X, MAX_POINT_Y],
-                geometry = Point((Longitude, Latitude)),
-                properties = {
-                    'country': Country,
-                    'mangroves': Mangroves
-                }
+                Feature(
+                    bbox=[MIN_POINT_X, MIN_POINT_Y, MAX_POINT_X, MAX_POINT_Y],
+                    geometry=Point((Longitude, Latitude)),
+                    properties={"country": Country, "mangroves": Mangroves},
+                )
             )
-        )
         else:
             pass
 
 collection = FeatureCollection(features)
 with open(json_file_path, "w") as f:
-    f.write('%s' % collection)
+    f.write("%s" % collection)
