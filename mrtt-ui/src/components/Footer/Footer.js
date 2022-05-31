1cc6aafd6a5c5cb5942a5f495e5968cc79f5e5d0
Brian Bancroft <hello@brianbancroft.ca>
2022-05-31 12:42:00 -0700

    Create styling for footer

--- mrtt-ui/src/components/Footer/Footer.js
+++ mrtt-ui/src/components/Footer/Footer.js
@@ -1,14 +1,59 @@
 import React from 'react'
 import { styled } from '@mui/system'
 import { Link } from 'react-router-dom'
+import {
+  Map as SitesIcon,
+  Language as LandscapesIcon,
+  PeopleOutline as OrganizationsIcon
+} from '@mui/icons-material'
 
-const StyledFooter = styled('footer')``
+const StyledFooter = styled('footer')`
+  display: flex;
+  justify-content: space-around;
+  padding-top: 0.2rem;
+  padding-bottom: 0.2rem;
+  border-top: 1px solid #009b93;
+  font-family: 'Open Sans', sans-serif;
+  text-transform: uppercase;
+  font-weight: 300;
+
+  a,
+  a:visited {
+    text-decoration: none;
+  }
+`
+
+const LinkContainer = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  color: ${(props) => (props.active ? '#00c6bd' : '#121212')};
+  div {
+    color: #121212;
+  }
+
+  border-bottom-width: 2px;
+  border-bottom-style: solid;
+
+  border-bottom-color: ${(props) => (props.active ? '#00c6bd' : 'transparent')};
+`
 
 function Footer() {
   return (
     <StyledFooter>
-      <div>StyledFooter</div>
-      <Link to='/'>home</Link>
+      <LinkContainer to='/sites' active={true}>
+        <SitesIcon />
+        <div>Sites</div>
+      </LinkContainer>
+      <LinkContainer to='/landscapes'>
+        <LandscapesIcon />
+        <div>landscapes</div>
+      </LinkContainer>
+      <LinkContainer to='/organizations'>
+        <OrganizationsIcon />
+        <div>organizations</div>
+      </LinkContainer>
     </StyledFooter>
   )
 }
