d0b54af1e1a0fe4c0089e699835aed0e806cb657
Brian Bancroft <hello@brianbancroft.ca>
2022-05-31 12:41:46 -0700

    Add page base-level components

---
+++ mrtt-ui/src/views/Landscapes.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+/** Scaffold component for the /landscapes route */
+function Landscapes() {
+  return <div>Landscapes (views/Landscapes)</div>
+}
+
+export default Landscapes
