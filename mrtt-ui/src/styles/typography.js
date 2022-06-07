a0b0d529f4e95dc0074225642a921a242d0b4a3c
Melissa Nunes <mnunes@sparkgeo.com>
2022-06-07 15:58:09 -0600

    [94] simplify front end routing link styles

--- mrtt-ui/src/styles/typography.js
+++ mrtt-ui/src/styles/typography.js
@@ -4,3 +4,5 @@ export const ErrorText = styled(Typography)`
   color: red;
 `
 ErrorText.defaultProps = { variant: 'subtitle' }
+
+export const Link = (props) => <LinkMui component={LinkReactRouter} {...props} />
