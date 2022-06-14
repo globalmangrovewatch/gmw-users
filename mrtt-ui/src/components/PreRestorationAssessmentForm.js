70196276e95169f615783d522078c56cb3b37607
julia-rom <jromanowski@sparkgeo.com>
2022-06-14 14:21:54 -0400

    Add first question and handle submit function

--- mrtt-ui/src/components/PreRestorationAssessmentForm.js
+++ mrtt-ui/src/components/PreRestorationAssessmentForm.js
@@ -1,5 +1,13 @@
-import React from 'react'
-import { FormLabel } from '@mui/material'
+import { useState } from 'react'
+import axios from 'axios'
+import {
+  useForm,
+  // useFieldArray,
+  Controller
+} from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import { FormLabel, MenuItem, TextField } from '@mui/material'
 
 import {
   Form,
@@ -11,15 +19,73 @@ import {
   //   SubTitle2
 } from '../styles/forms'
 // import { questionMapping } from '../data/questionMapping'
-import { preRestorationAssessment } from '../data/questions'
+import { preRestorationAssessment as questions } from '../data/questions'
+import { mapDataForApi } from '../library/mapDataForApi'
+import { ButtonSubmit } from '../styles/buttons'
+import { ErrorText } from '../styles/typography'
 
 function PreRestorationAssessmentForm() {
+  const validationSchema = yup.object().shape({
+    mangrovesPreviouslyOccured: yup.string().required('This field is required')
+  })
+  const reactHookFormInstance = useForm({
+    defaultValues: {},
+    resolver: yupResolver(validationSchema)
+  })
+  const {
+    handleSubmit: validateInputs,
+    formState: { errors },
+    control
+  } = reactHookFormInstance
+
+  const [isSubmitting, setisSubmitting] = useState(false)
+  const [isError, setIsError] = useState(false)
+
+  const handleSubmit = async (data) => {
+    setisSubmitting(true)
+    setIsError(false)
+
+    console.log('data: ', data)
+    const url = `${process.env.REACT_APP_API_URL}/sites/1/registration_answers`
+
+    if (!data) return
+
+    axios
+      .put(url, mapDataForApi('siteBackground', data))
+      .then(() => {
+        setisSubmitting(false)
+      })
+      .catch(() => {
+        setIsError(true)
+        setisSubmitting(false)
+      })
+  }
+
   return (
     <MainFormDiv>
       <SectionFormTitle>Pre-restoration Assessment</SectionFormTitle>
-      <Form>
+      <Form onSubmit={validateInputs(handleSubmit)}>
         <FormQuestionDiv>
-          <FormLabel>{preRestorationAssessment.mangrovesPreviouslyOccured.question}</FormLabel>
+          <FormLabel>{questions.mangrovesPreviouslyOccured.question}</FormLabel>
+          <Controller
+            name='mangrovesPreviouslyOccured'
+            control={control}
+            defaultValue=''
+            render={({ field }) => (
+              <TextField {...field} select value={field.value} label='select'>
+                {questions.mangrovesPreviouslyOccured.options.map((item, index) => (
+                  <MenuItem key={index} value={item}>
+                    {item}
+                  </MenuItem>
+                ))}
+              </TextField>
+            )}
+          />
+          <ErrorText>{errors.mangrovesPreviouslyOccured?.message}</ErrorText>
+        </FormQuestionDiv>
+        <FormQuestionDiv>
+          {isError && <ErrorText>Submit failed, please try again</ErrorText>}
+          <ButtonSubmit isSubmitting={isSubmitting}></ButtonSubmit>
         </FormQuestionDiv>
       </Form>
     </MainFormDiv>
