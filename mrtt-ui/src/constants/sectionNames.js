f6f2f0c0a94dfe8914ed161eea9cfff5fa6f25df
Melissa Nunes <mnunes@sparkgeo.com>
2022-07-14 16:55:11 -0600

    [205] feat: refine org list with respect to roles

---
+++ mrtt-ui/src/constants/sectionNames.js
@@ -0,0 +1,12 @@
+const SECTION_NAMES = [
+  'project-details',
+  'site-background',
+  'restoration-aims',
+  'causes-of-decline',
+  'pre-restoration-assessment',
+  'site-interventions',
+  'costs'
+]
+
+Object.freeze(SECTION_NAMES)
+export default SECTION_NAMES
