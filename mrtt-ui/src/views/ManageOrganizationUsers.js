9ba1f96edd752f39c5052f2c575b5cf9f4f439b9
Melissa Nunes <mnunes@sparkgeo.com>
2022-08-11 13:34:22 -0600

    feat: [206] manage org users: update org user role

--- mrtt-ui/src/views/ManageOrganizationUsers.js
+++ mrtt-ui/src/views/ManageOrganizationUsers.js
@@ -7,36 +7,81 @@ import { ButtonPrimary, ButtonSecondary } from '../styles/buttons'
 import { ContentWrapper, TitleAndActionContainer } from '../styles/containers'
 import { PageSubtitle, PageTitle } from '../styles/typography'
 import { TableAlertnatingRows, TdCenter, ThCenter } from '../styles/table'
+import { toast } from 'react-toastify'
+import { useAuth } from '../hooks/useAuth'
 import language from '../language'
+import LoadingIndicator from '../components/LoadingIndicator'
+import LoadingIndicatorOverlay from '../components/LoadingIndicatorOverlay'
 import USER_ROLES from '../constants/userRoles'
 
 const pageLanguage = language.pages.manageOrganizationUsers
 
 const ManageOrganizationUsers = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [isSubmitting, setIsSubmitting] = useState(false)
   const [organizationName, setOrganizationName] = useState()
   const [organizationUsers, setOrganizationUsers] = useState([])
+  const { currentUser } = useAuth()
   const { organizationId } = useParams()
   const organizationUrl = `${process.env.REACT_APP_API_URL}/organizations/${organizationId}`
   const organizationUsersUrl = `${organizationUrl}/users`
 
+  const updateOrganizationUser = (userWithUpdatedInfo) => {
+    const organizationUsersWithUserRoleUpdated = organizationUsers.map((existingUserInfo) => {
+      return existingUserInfo.id === userWithUpdatedInfo.id ? userWithUpdatedInfo : existingUserInfo
+    })
+    setOrganizationUsers(organizationUsersWithUserRoleUpdated)
+  }
+
   useEffect(
     function loadApiData() {
-      Promise.all([axios.get(organizationUrl), axios.get(organizationUsersUrl)]).then(
-        ([
-          {
-            data: { organization_name }
-          },
-          { data: organizationUsers }
-        ]) => {
-          setOrganizationName(organization_name)
-          setOrganizationUsers(organizationUsers)
-        }
-      )
+      Promise.all([axios.get(organizationUrl), axios.get(organizationUsersUrl)])
+        .then(
+          ([
+            {
+              data: { organization_name }
+            },
+            { data: organizationUsers }
+          ]) => {
+            setOrganizationName(organization_name)
+            setOrganizationUsers(organizationUsers)
+            setIsLoading(false)
+          }
+        )
+        .catch(() => {
+          setIsLoading(false)
+          toast.error(language.error.apiLoad)
+        })
     },
     [organizationUrl, organizationUsersUrl]
   )
-  return (
+
+  const handleUserRoleChange = ({
+    event: {
+      target: { value: newRole }
+    },
+    userEmail
+  }) => {
+    setIsSubmitting(true)
+    const orgUserUrl = `${organizationUsersUrl}/${userEmail}`
+
+    axios
+      .patch(orgUserUrl, { role: newRole })
+      .then(({ data: updatedUserInfo }) => {
+        setIsSubmitting(false)
+        updateOrganizationUser(updatedUserInfo)
+      })
+      .catch(() => {
+        setIsSubmitting(false)
+        toast.error(language.error.submit)
+      })
+  }
+
+  return isLoading ? (
+    <LoadingIndicator />
+  ) : (
     <>
+      <LoadingIndicatorOverlay isVisible={isSubmitting} />
       <ContentWrapper>
         <TitleAndActionContainer>
           <PageTitle>{pageLanguage.title} (Work in Progress!)</PageTitle>
@@ -52,34 +97,42 @@ const ManageOrganizationUsers = () => {
               <ThCenter>{pageLanguage.usersTable.user}</ThCenter>
               <ThCenter>{pageLanguage.usersTable.remove}</ThCenter>
             </tr>
-            {organizationUsers.map(({ id, name, role }) => (
-              <tr key={id}>
-                <td>{name}</td>
-                <TdCenter>
-                  <input
-                    type='radio'
-                    id={`${id}-${name}-role-admin`}
-                    name={`${id}-${name}-role`}
-                    value={USER_ROLES.orgAdmin}
-                    checked={role === USER_ROLES.orgAdmin}
-                  />
-                </TdCenter>
-                <TdCenter>
-                  <input
-                    type='radio'
-                    id={`${id}-${name}-role-user`}
-                    name={`${id}-${name}-role`}
-                    value={USER_ROLES.orgUser}
-                    checked={role === USER_ROLES.orgUser}
-                  />
-                </TdCenter>
-                <TdCenter>
-                  <ButtonSecondary>
-                    <GroupRemove />
-                  </ButtonSecondary>
-                </TdCenter>
-              </tr>
-            ))}
+            {organizationUsers.map(({ id, name, role, email }) => {
+              const isOrgUserLoggedIn = id === currentUser.id
+
+              return (
+                <tr key={id}>
+                  <td>{name}</td>
+                  <TdCenter>
+                    <input
+                      type='radio'
+                      id={`${id}-${name}-role-admin`}
+                      name={`${id}-${name}-role`}
+                      value={USER_ROLES.orgAdmin}
+                      checked={role === USER_ROLES.orgAdmin}
+                      onChange={(event) => handleUserRoleChange({ event, userEmail: email })}
+                      disabled={isOrgUserLoggedIn}
+                    />
+                  </TdCenter>
+                  <TdCenter>
+                    <input
+                      type='radio'
+                      id={`${id}-${name}-role-user`}
+                      name={`${id}-${name}-role`}
+                      value={USER_ROLES.orgUser}
+                      checked={role === USER_ROLES.orgUser}
+                      onChange={(event) => handleUserRoleChange({ event, userEmail: email })}
+                      disabled={isOrgUserLoggedIn}
+                    />
+                  </TdCenter>
+                  <TdCenter>
+                    <ButtonSecondary>
+                      <GroupRemove />
+                    </ButtonSecondary>
+                  </TdCenter>
+                </tr>
+              )
+            })}
           </tbody>
         </TableAlertnatingRows>
       </ContentWrapper>
