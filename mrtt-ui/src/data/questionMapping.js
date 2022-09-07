5ec0f33abfbb399c89094aa96f075b12b7e0d6ae
julia-rom <jromanowski@sparkgeo.com>
2022-09-07 11:56:23 -0400

    fix section 1 question mapping order + convert default date to iso string

--- mrtt-ui/src/data/questionMapping.js
+++ mrtt-ui/src/data/questionMapping.js
@@ -1,7 +1,7 @@
 export const questionMapping = {
   projectDetails: {
-    hasProjectEndDate: '1.1a',
-    projectStartDate: '1.1b',
+    projectStartDate: '1.1a',
+    hasProjectEndDate: '1.1b',
     projectEndDate: '1.1c',
     countries: '1.2',
     siteArea: '1.3'
