d17e0ee07d50dcf9ffa8f18f7830dddace8dd9c5
julia-rom <jromanowski@sparkgeo.com>
2022-09-07 13:31:14 -0400

    add delete confirm for 7.3

--- mrtt-ui/src/components/PreRestorationAssessment/PhysicalMeasurementRow.js
+++ mrtt-ui/src/components/PreRestorationAssessment/PhysicalMeasurementRow.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react'
-// import { TextField } from '@mui/material'
 import PropTypes from 'prop-types'
 import { Delete } from '@mui/icons-material'
+
+import ConfirmPrompt from '../ConfirmPrompt/ConfirmPrompt'
+import language from '../../language'
 
 import {
   LeftColumnDiv,
@@ -16,8 +18,10 @@ const PhysicalMeasurementRow = ({ label, value, unit, index, deleteItem, updateI
   const [currentValue, setCurrentValue] = useState('')
   const [initialUnit, setInitialUnit] = useState('')
   const [currentUnit, setCurrentUnit] = useState('')
+  const [isDeleteConfirmPromptOpen, setIsDeleteConfirmPromptOpen] = useState(false)
   const handleDelete = () => {
     deleteItem(index)
+    setIsDeleteConfirmPromptOpen(false)
   }
 
   const handleUpdate = () => {
@@ -43,6 +47,14 @@ const PhysicalMeasurementRow = ({ label, value, unit, index, deleteItem, updateI
         <LeftColumnDiv>
           <TabularLabel>{label}</TabularLabel>
           <Delete onClick={handleDelete}></Delete>
+          <ConfirmPrompt
+            isOpen={isDeleteConfirmPromptOpen}
+            setIsOpen={setIsDeleteConfirmPromptOpen}
+            title={`${language.form.tabularDeletePrompt.title}${TabularLabel}`}
+            promptText={language.form.tabularDeletePrompt.promptText}
+            confirmButtonText={language.form.tabularDeletePrompt.buttonText}
+            onConfirm={handleDelete}
+          />
         </LeftColumnDiv>
         <VerticalTabularBox>
           <RowTextField
