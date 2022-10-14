754833610b2995b8eef99c9dbc0374f33847cec2
julia-rom <jromanowski@sparkgeo.com>
2022-10-14 10:54:48 -0400

    show toast warning when form is saved with errors

--- mrtt-ui/src/components/FormValidationMessageIfErrors.js
+++ mrtt-ui/src/components/FormValidationMessageIfErrors.js
@@ -1,11 +1,20 @@
 import { Alert, AlertTitle } from '@mui/material'
 import PropTypes from 'prop-types'
-import React from 'react'
+import { useEffect } from 'react'
+import { toast } from 'react-toastify'
 
 import language from '../language'
 
 const FormValidationMessageIfErrors = ({ formErrors = {} }) => {
   const formHasErrors = !!Object.keys(formErrors).length
+
+  useEffect(() => {
+    if (formHasErrors) {
+      toast.warn(
+        `${language.form.validationAlert.title}. ${language.form.validationAlert.description}`
+      )
+    }
+  }, [formHasErrors])
 
   return formHasErrors ? (
     <Alert variant='outlined' severity='error'>
