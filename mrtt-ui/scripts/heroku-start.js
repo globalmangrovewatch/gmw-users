9ba47124565d872755d3a9c276166c386a043591
María <maluenarod@gmail.com>
2025-04-29 17:35:50 +0200

    heroku start

--- mrtt-ui/scripts/heroku-start.js
+++ mrtt-ui/scripts/heroku-start.js
@@ -12,9 +12,9 @@ app.use(express.json())
 
 app.use(express.static(path.join(__dirname, '..', 'build')))
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '..', 'build'))
-})
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, '..', 'build'))
+// })
 
 app.get('/{*splat}', async (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'build/index.html'))
